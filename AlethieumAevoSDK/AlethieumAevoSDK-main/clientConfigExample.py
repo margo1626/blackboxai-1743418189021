fc184a491d2b6852976ce99252540250e9411556
margo1626 <margo1626@tutamail.com>
2025-03-31 10:50:12 +0000

    Add AlethieumAevoSDK/AlethieumAevoSDK-main/clientConfigExample.py 1743418212317

---
+++ AlethieumAevoSDK/AlethieumAevoSDK-main/clientConfigExample.py
@@ -0,0 +1,11 @@
+### CLIENT CONFIG ###
+# # Description: Please add your API key and secret to the clientConfig file.
+# # Description: In the Readme.md file, you will find instructions on how to create an API key and secret.
+
+CLIENT_CONFIG = {
+    'signing_key': '',
+    'wallet_address': '',
+    'api_key': '',
+    'api_secret': '',
+    'env': '',
+}
